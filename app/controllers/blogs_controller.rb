6176ff162a74b875ce2d05a6ee20f9705451ad04
Cwaihenya <cwaihenya.gmail.com>
2021-06-14 17:13:45 +0900

    blogs new

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,11 @@
 class BlogsController < ApplicationController
-end
+  def create
+      @blog = current_user.blogs.build(blog_params)
+    #omit
+    end
+    #omit
+    def confirm
+      @blog = current_user.blogs.build(blog_params)
+      render :new if @blog.invalid?
+    end
+  end

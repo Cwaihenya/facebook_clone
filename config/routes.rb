012e4fe30e8f2502f80df3c72c7a4428cc1818f8
Cwaihenya <cwaihenya.gmail.com>
2021-06-14 15:37:04 +0900

    Blog controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :users, only: [:new, :create, :show, :edit, :destroy]# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:new, :create, :destroy]
+  resources :blogs
 end

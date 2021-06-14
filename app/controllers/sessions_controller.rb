32e4a1fa75973464d159576b242394ac286902e6
Cwaihenya <cwaihenya.gmail.com>
2021-06-14 13:58:16 +0900

    Sessions log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # If login is successful
+    else
+      flash.now[:danger] = 'Email address or password are incorrect, please try again'
+      render :new
+    end
+  end
 end
